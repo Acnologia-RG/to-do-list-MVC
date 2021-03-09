8ae634184e32e0a5e4674ea061c6072362ee7976
rens <rens@gouman.eu>
2021-03-09 14:07:13 +0100

    removed some hospital stuff i missed

--- view/templates/header.php
+++ view/templates/header.php
@@ -1,28 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>hospital</title>	
+	<title>To-Do List</title>	
 	<link rel="stylesheet" href="<?= URL ?>/public/css/style.css">
 	<meta charset="utf-8">
 </head>
 <body>
-	<nav>
-		<ul>
-			<li><a href="<?= URL ?>hospital/index">hospital</a></li>
-			<li><a href="<?= URL ?>todolist/index">to-do list</a></li>
-		</ul>
-	</nav>
-
-<?php if (isset($directory)) {
-		if ($directory == "hospital" ) { ?>
-		<h1>Hospital</h1>
-		<ul>
-			<li><a href="<?= URL ?>hospital/index">Patients</a></li>
-			<li><a href="<?= URL ?>hospital/clients">Clients</a></li>
-			<li><a href="<?= URL ?>hospital/species">Species</a></li>
-		</ul>	
-<?php  }} 
-
+<?php
 if (isset($directory)) {
 		if ($directory == "todolist" ) { ?>
 		<h1>to-do list</h1>
