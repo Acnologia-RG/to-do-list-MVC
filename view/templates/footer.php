bafbc19746b3feb46d69610412a9e0fa4c926f28
rens <rens@gouman.eu>
2021-03-09 14:08:04 +0100

    missed some more hospital

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,7 +1,2 @@
-<?php if (isset($directory)) {
-		if ($directory == "hospital" ) { ?>
-	<p><a href="<?= URL ?>hospital/index">Home</a></p>
-<?php  }} ?>
-<script type="text/javascript" src="<?= URL ?>public/js/sort.js"></script>
 </body>
 </html>
